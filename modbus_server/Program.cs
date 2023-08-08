c5bc6a0af78c5cb581146ae0519131b1b947baaf
将貴 米川 <masaki.2.28.my.f@gmail.com>
2023-08-09 03:50:14 +0900

    プロジェクト ファイルを追加します。

---
+++ modbus_server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace modbus_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 502); // Modbusのデフォルトポートは502
+            listener.Start();
+
+            Console.WriteLine("Modbus TCP Server started.");
+
+            using (TcpClient client = listener.AcceptTcpClient())
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    // データの処理とレスポンスの生成
+                    // Modbusプロトコルに従ってレスポンスデータを作成する
+                    byte[] responseData = GenerateModbusResponse(buffer, bytesRead);
+
+                    // レスポンスをクライアントに送信
+                    stream.Write(responseData, 0, responseData.Length);
+                }
+            }
+
+            listener.Stop();
+        }
+        static byte[] GenerateModbusResponse(byte[] requestData, int length)
+        {
+            Console.WriteLine("serverdata: " + BitConverter.ToString(requestData));
+            // ここでModbusのリクエストデータを解析し、適切なレスポンスデータを生成するロジックを実装
+            // responseDataに生成したレスポンスデータを格納して返す
+            byte[] responseData = new byte[length];
+            Array.Copy(requestData, responseData, length);
+            return responseData;
+        }
+    }
+}
